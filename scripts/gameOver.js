aff75bc62e3b64466119bef4033316e8f5deb9b4
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2020-06-26 03:34:02 -0300

    change sound gameover

--- scripts/gameOver.js
+++ scripts/gameOver.js
@@ -1,6 +1,8 @@
 function restart(){
     isDead = false
     score.score = 0;
+    soundGameOver.stop();
+    soundCity.loop();
     boy.y  = height - boy.charHeight - boy.charY;
     enemies[currentEnemy].charX = -10*enemies[currentEnemy].charHeight
     loop();
