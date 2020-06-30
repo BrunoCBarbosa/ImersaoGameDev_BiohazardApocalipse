f2f3b34c7794df1044c51f8e847f87d0bc441898
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2020-06-30 19:56:44 -0300

    fixed intro text

--- scripts/scenes/intro.js
+++ scripts/scenes/intro.js
@@ -40,17 +40,17 @@ class Intro{
         }
         if(next == 1){
             text("A day, an weird explosion ocurred in that industry,", width/2, height/4);
-            text("covering all the city with a black and bih smoke,", width/2, height/3);
-            text("and spreading its waste into the swears.", width/2, height/2.5);
+            text("covering all the city with a black and big smoke,", width/2, height/3);
+            text("and spreading its waste, into the swears.", width/2, height/2.5);
             text("In a few hours all the citizens, start to feel bad.", width/2, height/2);
         }
         if(next == 2){
             text("Filling all the hospitals. But in in a few hours,", width/2, height/4);
-            text("inexplicably some people start to die,", width/2, height/3);
-            text("but not completely, until the night, they start to get up.", width/2, height/2.5);
+            text("inexplicably, some people start to die,", width/2, height/3);
+            text("but not completely, during the night, they got up.", width/2, height/2.5);
         }
         if(next == 3){
-            text("Turned them into undead, ating all the citizens, and infecting them,", width/2, height/4);
+            text("Turned them into undead, eating all the citizens, and infecting them,", width/2, height/4);
             text("begining a caos, and the end of Squire City.", width/2, height/3);
             text("Now, you're the last survivor, and must escape from this terror", width/2, height/2.5);
             text("as hurry as you can.", width/2, height/2);
